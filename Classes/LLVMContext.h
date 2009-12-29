efb5e632a086f8a6b870bceecfea4182e8a3f5d5
Rob Rix <rob@monochromeindustries.com>
2009-12-29 03:33:17 -0500

    Initial commit.

---
+++ Classes/LLVMContext.h
@@ -0,0 +1,11 @@
+// LLVMContext.h
+// Created by Rob Rix on 2009-12-29
+// Copyright 2009 Monochrome Industries
+
+@interface LLVMContext : NSObject {
+	
+}
+
++(LLVMContext *)sharedContext; // doesn’t have to be used
+
+@end
---
+++ Classes/LLVMContext.m
@@ -0,0 +1,11 @@
+// LLVMContext.m
+// Created by Rob Rix on 2009-12-29
+// Copyright 2009 Monochrome Industries
+
+#import "LLVMContext.h"
+
+@implementation LLVMContext
+
+
+
+@end
