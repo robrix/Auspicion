ee872607324404a4c07df67ad9019f32f8915315
Rob Rix <rob@monochromeindustries.com>
2009-12-30 02:24:47 -0500

    Added 8-, 16-, 32-, and 64-bit integer types.

--- Classes/LLVMType.h
+++ Classes/LLVMType.h
@@ -8,7 +8,13 @@
 
 @interface LLVMType : NSObject
 
+// integer type is 64-bit on __LP64__, 32-bit otherwise, just like NSInteger/NSUInteger
 +(LLVMType *)integerTypeInContext:(LLVMContext *)context;
+
++(LLVMType *)int8TypeInContext:(LLVMContext *)context;
++(LLVMType *)int16TypeInContext:(LLVMContext *)context;
++(LLVMType *)int32TypeInContext:(LLVMContext *)context;
++(LLVMType *)int64TypeInContext:(LLVMContext *)context;
 
 +(LLVMType *)pointerInContext:(LLVMContext *)context toType:(LLVMType *)type addressSpace:(NSUInteger)addressSpace;
 
