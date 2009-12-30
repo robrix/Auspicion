02621e47b8f5a2ba479dcf20a33155fb54c3c63a
Rob Rix <rob@monochromeindustries.com>
2009-12-30 01:15:22 -0500

    Added a framework header.

---
+++ Other Sources/Auspicion.h
@@ -0,0 +1,12 @@
+// Auspicion.h
+// Created by Rob Rix on 2009-12-30
+// Copyright 2009 Monochrome Industries
+
+#import <Foundation/Foundation.h>
+
+#import <Auspicion/LLVMBuilder.h>
+#import <Auspicion/LLVMContext.h>
+#import <Auspicion/LLVMFunction.h>
+#import <Auspicion/LLVMModule.h>
+#import <Auspicion/LLVMType.h>
+#import <Auspicion/LLVMValue.h>
