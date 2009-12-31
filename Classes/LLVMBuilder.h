ab3b108604f905a55e70e8b6bb3e8392a2ba682e
Rob Rix <rob@monochromeindustries.com>
2009-12-31 12:12:32 -0500

    Simplified the builder API with varargs (prefix-style) methods, e.g. [builder and: left, right], and [builder call: function, arg, arg, nil].

--- Classes/LLVMBuilder.h
+++ Classes/LLVMBuilder.h
@@ -6,31 +6,37 @@
 
 @class LLVMContext, LLVMFunction, LLVMValue;
 
+/*
+Unless otherwise noted (e.g. with NS_REQUIRES_NIL_TERMINATION), variadic builder methods take exactly two arguments. For example, -add: is used like so:
+
+[builder add: left, right];
+*/
+
 @interface LLVMBuilder : NSObject
 
 +(LLVMBuilder *)builderWithContext:(LLVMContext *)context;
 
-
 -(void)positionAtEndOfFunction:(LLVMFunction *)function;
 
-// I am not happy with these at all
+// can these be improved upon?
 -(LLVMValue *)return:(LLVMValue *)value;
 
 -(LLVMValue *)call:(LLVMFunction *)function arguments:(NSArray *)arguments;
 -(LLVMValue *)call:(LLVMFunction *)function argument:(LLVMValue *)argument;
+-(LLVMValue *)call:(LLVMFunction *)function, ... NS_REQUIRES_NIL_TERMINATION;
 
 -(LLVMValue *)condition:(LLVMValue *)condition then:(LLVMValue *)thenValue else:(LLVMValue *)elseValue;
 
--(LLVMValue *)add:(LLVMValue *)left to:(LLVMValue *)right;
+-(LLVMValue *)add:(LLVMValue *)left, ...;
 -(LLVMValue *)not:(LLVMValue *)value;
 
 -(LLVMValue *)stringPointer:(NSString *)string;
 
 -(LLVMValue *)offsetPointer:(LLVMValue *)pointerValue by:(LLVMValue *)offsetValue;
 
--(LLVMValue *)valueIsTrue:(LLVMValue *)left andValueIsTrue:(LLVMValue *)right;
+-(LLVMValue *)and:(LLVMValue *)left, ...;
 
--(LLVMValue *)unsignedValue:(LLVMValue *)left isLessThanOrEqualTo:(LLVMValue *)right;
--(LLVMValue *)value:(LLVMValue *)left isEqualTo:(LLVMValue *)right;
+-(LLVMValue *)unsignedLessOrEqual:(LLVMValue *)left, ...;
+-(LLVMValue *)equal:(LLVMValue *)left, ...;
 
 @end
