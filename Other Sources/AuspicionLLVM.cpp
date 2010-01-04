e7fba6d260fb5a04da18a947c01ccde63df541c3
Rob Rix <rob@silver.local>
2010-01-04 11:27:45 -0500

    Added a function to retrieve an LLVMFunctionRef’s function type.

--- Other Sources/AuspicionLLVM.cpp
+++ Other Sources/AuspicionLLVM.cpp
@@ -5,9 +5,16 @@
 #include "AuspicionLLVM.h"
 
 #include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include "llvm/Function.h"
 
 void *AuspicionLLVMGetPointerToFunction(LLVMExecutionEngineRef compiler, LLVMValueRef function) {
 	llvm::ExecutionEngine *cppCompiler = llvm::unwrap(compiler);
 	llvm::Function *cppFunction = llvm::unwrap<llvm::Function>(function);
 	return cppCompiler->getPointerToFunction(cppFunction);
+}
+
+LLVMTypeRef AuspicionLLVMGetFunctionType(LLVMValueRef function) {
+	llvm::Function *cppFunction = llvm::unwrap<llvm::Function>(function);
+	const llvm::FunctionType *cppType = cppFunction->getFunctionType();
+	return llvm::wrap(cppType);
 }
