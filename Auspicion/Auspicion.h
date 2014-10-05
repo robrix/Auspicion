262c1a52d57ae244d21a97810ff555ecd6c03c08
Rob Rix <rob.rix@github.com>
2014-10-05 13:43:21 -0400

    Project.

---
+++ Auspicion/Auspicion.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+/// Project version number for Auspicion.
+extern double AuspicionVersionNumber;
+
+/// Project version string for Auspicion.
+extern const unsigned char AuspicionVersionString[];
+
