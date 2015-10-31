27e2ddd08dab427c48ffbfffd0e5b41c85454fbf
Rob Rix <rob.rix@github.com>
2015-10-31 18:35:43 -0400

    Extend LLVMTypeRef to be constructed with a Type and LLVMContextRef.

--- Auspicion/LLVM.swift
+++ Auspicion/LLVM.swift
@@ -1,4 +1,16 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
+extension LLVMTypeRef {
+	init(type: Type, context: LLVMContextRef) {
+		switch type {
+		case .Void:
+			self = LLVMVoidType()
+		default:
+			print("unimplemented")
+			self = LLVMVoidType()
+		}
+	}
+}
+
 
 import llvm
