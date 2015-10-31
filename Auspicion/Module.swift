2be965e8ee8eab0fc7f4684700d50d5b5b8f3499
Rob Rix <rob.rix@github.com>
2015-10-31 19:30:32 -0400

    Module is a value type.

--- Auspicion/Module.swift
+++ Auspicion/Module.swift
@@ -1,21 +1,10 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-import llvm
-
 /// A module, i.e. a single unit of compilation.
-public final class Module {
+public struct Module {
 	let name: String
-	let context: Context
 
-	init(name: String, context: Context) {
+	init(name: String) {
 		self.name = name
-		self.context = context
-		self.module = LLVMModuleCreateWithNameInContext(name, context.context)
 	}
-
-	deinit {
-		LLVMDisposeModule(module)
-	}
-
-	private let module: LLVMModuleRef
 }
