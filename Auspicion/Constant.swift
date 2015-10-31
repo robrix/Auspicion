6e24db92c595bdd8d576f290ec09154683f1e892
Rob Rix <rob.rix@github.com>
2015-10-31 19:02:38 -0400

    Constant is BooleanLiteralConvertible.

--- Auspicion/Constant.swift
+++ Auspicion/Constant.swift
@@ -1,5 +1,12 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-enum Constant {
+enum Constant: BooleanLiteralConvertible {
 	case Boolean(Bool)
+
+
+	// MARK: BooleanLiteralConvertible
+
+	init(booleanLiteral: Bool) {
+		self = .Boolean(booleanLiteral)
+	}
 }
